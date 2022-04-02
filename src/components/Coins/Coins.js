ce9c14b3c1aace61a995546eea833d4f342f9e5b
sdshuvo98 <hellosdshuvo@gamil.com>
2022-04-02 23:09:12 +0600

    detailspage what is coinname

--- src/components/Coins/Coins.js
+++ src/components/Coins/Coins.js
@@ -4,7 +4,7 @@ import Coin from '../Coin/Coin';
 const Coins = ({ addToCart }) => {
     const [coins, setCoins] = useState([])
     useEffect(() => {
-        fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false')
+        fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false')
             .then(res => res.json())
             .then(data => setCoins(data))
     }, [coins])
--- src/components/DetailsPage/DetailsPage.js
+++ src/components/DetailsPage/DetailsPage.js
@@ -13,27 +13,62 @@ const DetailsPage = ({ addToCart }) => {
     }, [coin, id])
 
     return (
-        <div className='text-left px-[100px]'>
-            <div className='grid grid-cols-2'>
-                <div>
-                    <p className='font-mono bg-slate-800 p-1 rounded-sm'>Rank#{coin.market_cap_rank}</p>
-                    <div className='flex gap-2 my-3'>
-                        <img src={coin.image?.thumb} alt="" />
-                        <p className='text-xl font-semibold'>{coin.name} ({coin.symbol})</p>
+        <div className='text-left px-[100px] flex items-center min-h-[90vh]'>
+            <div>
+                <div className='grid grid-cols-2 items-center'>
+                    <div>
+                        <p><small className='bg-slate-800 px-3 py-1 rounded-md'>Rank#{coin.market_cap_rank}</small></p>
+                        <div className='flex gap-2 my-3'>
+                            <img src={coin.image?.thumb} alt="" />
+                            <p className='text-xl font-semibold'>{coin.name} ({coin.symbol})</p>
+                        </div>
+                        <div className='flex items-center gap-3'>
+                            <p className='text-4xl font-semibold'>${(coin.market_data?.current_price?.usd)?.toLocaleString("en-US")}</p>
+                            <p className={`flex justify-center items-center ${coin.market_data?.price_change_percentage_24h >= 0 ? 'text-green-600' : 'text-red-700'} font-semibold`}>{coin.market_data?.price_change_percentage_24h >= 0 ? <ArrowSmUpIcon className="h-5 w-5 text-green-500" /> : <ArrowSmDownIcon className="h-5 w-5 text-red-500" />}{(coin.market_data?.price_change_percentage_24h)?.toFixed(2)}%</p>
+                        </div>
+                        <div className='flex gap-3 my-1 opacity-50'>
+                            <p><small>24h High: ${(coin.market_data?.high_24h?.usd)?.toLocaleString("en-US")}</small></p>
+                            <p><small>24h Low: ${(coin.market_data?.low_24h?.usd)?.toLocaleString("en-US")}</small></p>
+                        </div>
+                        <button onClick={() => addToCart(coin)} className='text-base border border-rose-500 rounded-md py-2 px-10 hover:bg-rose-500 my-3'>Buy now</button>
                     </div>
-                    <div className='flex items-center gap-3'>
-                        <p className='text-4xl font-semibold'>${(coin.market_data?.current_price?.usd)?.toLocaleString("en-US")}</p>
-                        <p className={`flex justify-center items-center ${coin.market_data?.price_change_percentage_24h >= 0 ? 'text-green-600' : 'text-red-700'} font-semibold`}>{coin.market_data?.price_change_percentage_24h >= 0 ? <ArrowSmUpIcon className="h-5 w-5 text-green-500" /> : <ArrowSmDownIcon className="h-5 w-5 text-red-500" />}{(coin.market_data?.price_change_percentage_24h)?.toFixed(2)}%</p>
+                    <div>
+                        <h2 className='text-xl font-bold opacity-80'>Info </h2>
+                        <div className='grid grid-cols-2 gap-5 opacity-80'>
+                            <div>
+                                <div className='flex justify-between py-3 border-b'>
+                                    <p>Market Cap </p>
+                                    <p className='font-medium'>${(coin.market_data?.market_cap?.usd)?.toLocaleString("en-US")}</p>
+                                </div>
+                                <div className='flex justify-between py-3 border-b'>
+                                    <p>24h Trading Vol </p>
+                                    <p className='font-medium'>${(coin.market_data?.total_volume?.usd)?.toLocaleString("en-US")}</p>
+                                </div>
+                                <div className='flex justify-between py-3 border-b'>
+                                    <p>F.D.V</p>
+                                    <p className='font-medium'>${(coin.market_data?.fully_diluted_valuation?.usd ? coin.market_data?.fully_diluted_valuation?.usd : 'Not found')?.toLocaleString("en-US")}</p>
+                                </div>
+                            </div>
+                            <div>
+                                <div className='flex justify-between py-3 border-b'>
+                                    <p>Circulating Supply</p>
+                                    <p className='font-medium'>{(coin.market_data?.circulating_supply)?.toLocaleString("en-US")}</p>
+                                </div>
+                                <div className='flex justify-between py-3 border-b'>
+                                    <p>Total Supply</p>
+                                    <p className='font-medium'>{(coin.market_data?.total_supply)?.toLocaleString("en-US")}</p>
+                                </div>
+                                <div className='flex justify-between py-3 border-b'>
+                                    <p>Max Supply</p>
+                                    <p className='font-medium'>{(coin.market_data?.max_supply ? coin.market_data?.max_supply : 'Not found')?.toLocaleString("en-US")}</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className='flex gap-3 my-1 font-thin'>
-                        <p><small>24h High: ${(coin.market_data?.high_24h?.usd)?.toLocaleString("en-US")}</small></p>
-                        <p><small>24h Low: ${(coin.market_data?.low_24h?.usd)?.toLocaleString("en-US")}</small></p>
-                    </div>
-                    <button onClick={() => addToCart(coin)} className='text-base border border-rose-500 rounded-md py-2 px-10 hover:bg-rose-500 my-3'>Buy now</button>
                 </div>
-                <div>
-                    <h2>Info: </h2>
-                    <p>Market Cap : <span className='font-medium'>${(coin.market_data?.market_cap?.usd)?.toLocaleString("en-US")}</span></p>
+                <div className='mt-10'>
+                    <h1 className='text-xl font-bold my-2'>What is {coin.name} ?</h1>
+                    <p className='text-justify opacity-70 font-light'> {(coin.description?.en)?.slice(0, 500)}. </p>
                 </div>
             </div>
         </div>
